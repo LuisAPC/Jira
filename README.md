06d6af441669207c160903b88f0174c03a933eef
lplancarte <lplancarte@outlook.com>
2023-08-06 12:08:43 -0600

    Jira useful info

---
+++ README.md
@@ -0,0 +1,6 @@
+#Jira
+
+##Setting up this tool
+
+Using jira as a project management tool for setting up
+your software development planning
---
+++ intro.py
@@ -0,0 +1,51 @@
+"""
+----------Introduction to Jira:----------
+Project an issue tracking software (pretty much agile based software)
+Project management tool
+
+It has a lot of coordination and planning that you cannot do in other
+    softwares (fine tunning)
+From company attlassian: Jira software
+
+Most common use cases:
+If you have work to be done you can use jira to manage it
+For requirements & test case management (providing visibility)
+For practicing agile methodologies
+For tight integration with ci/cd tools
+    jenkins, repositories
+    you have an entire view of the pipeline of your software deliverable
+
+Creating a project management workflow for a software proces:
+    Create an agile process:
+        functionality - planning - implementation - testing - review
+
+1. Create project
+
+2. Go to backlog -> + sign (to create issue)
+Issue: element within your project workflow (task, bug, story)
+    issues can be choosen between Story, Task, Bug or Epic
+
+3. Always start with an Epic
+Epic: Abstract view of what we need to create for our clients
+
+4. Select Story
+Story: What is required to achieve your Epic
+    So basically all the features that your Epic needs and then you can
+    create tasks to complete this story
+
+5. Create Tasks to complete the sories
+
+6. Create sprint (project workflow)
+    Drag Stories to the sprint
+    Click on start sprint -> change whatever you need -> click start
+
+7. Click on any story to modify it, create child issues
+
+Scrum Role:
+    Hierarchy of employees working on a Software Dev project and how
+    they function to achieve agile development
+        Project owner
+        Scrum Master (manager)
+        Scrum team (employees)
+
+"""
